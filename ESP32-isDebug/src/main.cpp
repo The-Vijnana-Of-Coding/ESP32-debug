5535c8572e67d52f7b4ff3cda0510283a0e51d11
Mark Alan Ralph Caton <100426775+unknown-brahman@users.noreply.github.com>
2022-10-16 17:38:33 +0100

    initial commit

---
+++ ESP32-isDebug/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#include <isDebug.h> // change isDebug to true to enable print, false to disable
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+    debugln("This is a new line print");
+    debug("This is an inline print");
+    debugln("This is a new line print again");
+    delay(1000);
+}
